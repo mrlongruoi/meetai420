fe23f1ed719c65cb2525aae750d4c6d530f9fe4b
mrlongruoi <mrlong01012020@gmail.com>
2025-05-28 13:51:13 +0700

    04: authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view"
+
+
+const Page = () => {
+  return (
+
+        <SignInView/>
+  )
+}
+
+export default Page
+
+// localhost:3000/auth/sign-in
