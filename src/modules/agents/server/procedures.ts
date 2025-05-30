32640467f955833f001e87f17121d7ec38ba5f24
mrlongruoi <mrlong01012020@gmail.com>
2025-05-30 14:53:10 +0700

    12: agents data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,32 +1,23 @@
 import { z } from "zod";
 import { db } from "@/db";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 import { agents } from "@/db/schema";
 import {createTRPCRouter, protectedProcedure} from "@/trpc/init"
 import { agentsInsertSchema } from "../schemas";
-// import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
-    //TODO: Change `getOne` to use `protectedProcedure`
     getOne: protectedProcedure.input(z.object({id: z.string()})).query(async ({input}) => {
         const [existingAgent] = await db
-        .select()
+        .select({
+            meetingCount: sql<number>`5`,
+            ...getTableColumns(agents),
+        })
         .from(agents)
         .where(eq(agents.id, input.id))
-        
-
-        // await new Promise((resolve) => setTimeout(resolve, 5000));
-        // throw new TRPCError({code: "BAD_REQUEST"});
-
         return existingAgent;
     }),
     getMany: protectedProcedure.query(async () => {
         const data = await db.select().from(agents);
-        
-
-        // await new Promise((resolve) => setTimeout(resolve, 5000));
-        // throw new TRPCError({code: "BAD_REQUEST"});
-
         return data;
     }),
     create: protectedProcedure.input(agentsInsertSchema).mutation(async ({input, ctx}) => {
