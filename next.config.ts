a8e7c3f9770fe8433321a831c20aa7c59708b1f1
mrlongruoi <mrlong01012020@gmail.com>
2025-06-11 21:01:16 +0700

    fix functions inngest

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/meetings",
+  //       permanent: true,
+  //     }
+  //   ]
+  // }
 };
 
 export default nextConfig;
